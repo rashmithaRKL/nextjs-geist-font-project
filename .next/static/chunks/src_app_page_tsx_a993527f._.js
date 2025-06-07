0cbe84abccd6896cf2a559c0412f83030141018f
Rashmitha Kalhara <rashmithakalhara2001@gmail.com>
2025-06-07 23:26:21 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1749318985161

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7fb24c9c._.js",
+  "static/chunks/src_345dbf0a._.js"
+],
+    source: "dynamic"
+});
